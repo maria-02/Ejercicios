b92158d892ea43d61c6d503a5ace20ac5f4e7236
maria-02 <69777624+maria-02@users.noreply.github.com>
2022-06-15 17:32:24 -0300

    Parte normal ej 3

--- clase12/js/app.js
+++ clase12/js/app.js
@@ -1,17 +1,42 @@
 $(function () {
 	let arrayMarcas = ["BMW", "Peugeot", "Chevrolet", "Subaru", "Nissan"];
-	$("#mostrar").on("click", function () {
-		$("ul").empty();
-		arrayMarcas.forEach((item) => {
-			$(".lista").append($("<li>").text(item));
+	function mostrarMarcas(array) {
+		$("#mostrar").on("click", function () {
+			$("ul").empty();
+			array.forEach((item) => {
+				$(".lista").append($("<li>").text(item));
+			});
 		});
-	});
+	}
+
+	mostrarMarcas(arrayMarcas);
+
 	$("#eliminar").on("click", function () {
 		arrayMarcas.pop();
 		$("li:last").remove();
 		if (arrayMarcas.length === 0) {
 			$(".alert").show();
+
+			$("#volver").show();
 			$(".row").fadeTo(500, 0.5);
+		}
+	});
+
+	$("#volver").on("click", function () {
+		$(".alert").hide();
+		$("#volver").hide();
+		$(".row").fadeTo(500, 1);
+	});
+
+	$("#agregar").on("click", function () {
+		let nuevo = $("#agregarMarca").val();
+		if (!arrayMarcas.includes(nuevo)) {
+			arrayMarcas.push(nuevo);
+			$(".lista").append($("<li>").text(nuevo));
+		} else {
+			$("#volver").show();
+			$(".row").fadeTo(500, 0.5);
+			$(".alert").show();
 		}
 	});
 });
